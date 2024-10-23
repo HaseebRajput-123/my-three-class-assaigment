3952a63e58bc86920498db7da4d52c052a4da437
haseeb rajput <haseebrajput059@gmail.com>
2024-10-23 08:00:37 +0500

    three-class

---
+++ src/app/Contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Contact(){
+    return(
+        <>
+        <h1>This is Contact page</h1>
+        <h2>My Name is Haseeb Rajput</h2>
+        <h3><b>phone:</b>03182029689</h3>
+        </>
+    )
+}
